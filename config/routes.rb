c41cf6d699cb819010253d83cb3aac9cdf969955
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-03-23 16:22:24 +0900

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   root 'blogs#home'
   get 'about', to: 'blogs#about'
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
