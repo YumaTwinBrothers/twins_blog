939ed5c406faf425e4fd159a5ca72076f48160f4
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-03-24 21:25:05 +0900

    Add helper method for authentication in application controller and set before action in user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # not to conect to model and database every time doing something by user, need to define that like to directly get whether user is current or not not to be delay
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 end
